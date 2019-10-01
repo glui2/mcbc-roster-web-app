aaef97375eb598f01c04c5fdb220a1d04b0210c5
Dragon <dragon88power@gmail.com>
2019-10-01 14:44:13 +1000

    minor text fixes, more responsive
    Made minor text fixes, design is more responsive. Vertical centering not working

--- django-drf-react-quickstart/mcbcapp/frontend/src/components/IntroPage.js
+++ django-drf-react-quickstart/mcbcapp/frontend/src/components/IntroPage.js
@@ -1,49 +1,41 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-//import logo from "../assets/icons/logo.png";
-
-//console.log(logo);
 
 class IntroPage extends Component{ 
-    render(){
-               
-            const backgroundStyle = {
-                backgroundColor: "#32669d", 
-                width: "1919.9px", 
-                height: "1080px"
-            };
-            const nameStyle1 = {
-                color: "white",
-                width: "390px",
-                height: "100.8",
-                fontSize:"50px",  
-                position: "center" 
+    render(){                   
+        const backgroundStyle = {
+            backgroundColor: "#32669D",
+            //alignItems: "center",
+            margin: "auto",
+            width: "100%",
+            height: "100%",
+            textAlign: "center",
+            alignItems: "center" // not working
+        };
+        const nameStyle1 = {
+            color: "white",
+            fontSize: "7.5vw",               
+        };
+        const nameStyle2 = {
+            color: "white", 
+            fontSize: "3vw",            
+        };
+        return(
+            <div style={backgroundStyle}>
                 
-            };
-            const nameStyle2 = {
-                color: "white", 
-                width:"281px",
-                height:"62.9px",
-                fontSize:"20px",  
-                position: "center"
-            };
+                <img src={"/static/assets/icons/logo.png"} 
+                    alt = "app logo" 
+                    width= "20%" 
+                    height= "24%"
+                    />
+                <h1 style = {nameStyle1}> <br></br>
+                    CROSSGEN 
+                </h1>
+                <h2 style = {nameStyle2}>
+                    ROSTER
+                </h2>                                      
 
-            return(
-    
-            <body style={backgroundStyle}>
-                
-                <center>
-                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-
-                    <img src={"/static/assets/icons/logo.png"} alt = "app logo" width="182" height="220" />
-                    <h1 style = {nameStyle1}> <br></br>
-                        CROSSGEN 
-                    </h1>
-                    <h2 style = {nameStyle2}>
-                        ROSTER
-                    </h2>                                      
-                </center>
-            </body>
+            </div>
         );  
         
         
