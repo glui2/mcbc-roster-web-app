11ff19742a4dba6bec778daac1d526098e740fe8
Dragon <dragon88power@gmail.com>
2019-10-02 18:13:56 +1000

    Responsive design
    Implemented centering and it is more responsive to design

--- django-drf-react-quickstart/mcbcapp/frontend/src/components/IntroPage.js
+++ django-drf-react-quickstart/mcbcapp/frontend/src/components/IntroPage.js
@@ -5,42 +5,50 @@ class IntroPage extends Component{
     render(){                   
         const backgroundStyle = {
             backgroundColor: "#32669D",
-            //alignItems: "center",
-            margin: "auto",
-            width: "100%",
-            height: "100%",
-            textAlign: "center",
-            alignItems: "center" // not working
+            width: "100vw",
+            height: "100vh",
         };
+        const divStyle = {
+            height: "35%",
+            width: "60%",
+            position: "absolute",
+            top: "25%",
+            left: "20%"
+
+        }
         const nameStyle1 = {
             color: "white",
-            fontSize: "7.5vw",               
+            fontSize: "7.5vw", 
+            textAlign: "center"              
         };
         const nameStyle2 = {
             color: "white", 
-            fontSize: "3vw",            
+            fontSize: "3vw",
+            textAlign: "center"              
         };
+        const imgStyle = {
+            display: "block",
+            margin: "auto"
+        }
         return(
-            <div style={backgroundStyle}>
-                
-                <img src={"/static/assets/icons/logo.png"} 
-                    alt = "app logo" 
-                    width= "20%" 
-                    height= "24%"
+            <html style={backgroundStyle}>
+                <div style = {divStyle}>            
+                    <img src = {"/static/assets/icons/logo.png"} 
+                        alt = "app logo" 
+                        width = "25%" 
+                        height = "57.14%"
+                        style = {imgStyle}
                     />
-                <h1 style = {nameStyle1}> <br></br>
-                    CROSSGEN 
-                </h1>
-                <h2 style = {nameStyle2}>
-                    ROSTER
-                </h2>                                      
-
-            </div>
-        );  
-        
-        
+                    <h1 style = {nameStyle1}>
+                        CROSSGEN 
+                    </h1>
+                    <h2 style = {nameStyle2}>
+                        ROSTER
+                    </h2>                                      
+                </div> 
+            </html>
+        );            
     }
-
 }
 
 export default IntroPage;
