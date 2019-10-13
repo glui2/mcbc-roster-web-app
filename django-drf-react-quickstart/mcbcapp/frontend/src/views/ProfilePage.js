4f4b1838a8187e26abd36e5d9eac42ba2f1f23bf
Gavin <gavin.lui@gmail.com>
2019-10-14 00:40:38 +1100

    added elements to profile page
    Added:
    - Server's name
    - icons
    
    TODO:
    - background image
    -fix height

--- django-drf-react-quickstart/mcbcapp/frontend/src/views/ProfilePage.js
+++ django-drf-react-quickstart/mcbcapp/frontend/src/views/ProfilePage.js
@@ -1,5 +1,61 @@
 import React from "react";
+import { makeStyles, useTheme, Typography } from "@material-ui/core";
+import color from "@material-ui/core/colors/blueGrey";
+
+const profileStyles = makeStyles(theme => ({
+  backgroundStyling: {
+    backgroundImage: "/static/assets/images/background_profile_page.jpg",
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    height: "-webkit-fill-available"
+  },
+  usernameBox: {
+    borderStyle: "solid",
+    borderColor: "#E53232",
+    margin: "auto",
+    marginTop: "5%",
+    marginBottom: "5%",
+    padding: "5%",
+    paddingLeft: "8%",
+    paddingRight: "8%"
+  },
+  ministryIcon: {
+    width: "10%",
+    margin: "3%"
+  }
+}));
 
 export default function ProfilePage() {
-  return <h1>Profile Page</h1>;
+  const profileClasses = profileStyles();
+  const theme = useTheme();
+
+  return (
+    <div className={profileClasses.backgroundStyling}>
+      <div>
+        <img
+          style={{ width: "5%" }}
+          src="/static/assets/icons/ProfileIcon2x.png"
+        />
+      </div>
+      <div className={profileClasses.usernameBox}>
+        <Typography style={{ color: "#E53232" }} variant="h2">
+          Chris <br /> Yeung
+        </Typography>
+      </div>
+      <Typography style={{ color: "#E53232" }} variant="h6">
+        I AM SERVING IN
+      </Typography>
+      <div>
+        <img
+          src="/static/assets/icons/WorshipIcon.png"
+          className={profileClasses.ministryIcon}
+        />
+        <img
+          src="/static/assets/icons/AVIcon.png"
+          className={profileClasses.ministryIcon}
+        />
+      </div>
+    </div>
+  );
 }
