51da8097dd5c90aea66fbdea398afd718c03735a
Gavin <gavin.lui@gmail.com>
2019-12-10 17:39:09 +1100

    removed unused components

--- django-drf-react-quickstart/mcbcapp/frontend/src/components/Header.js
+++ django-drf-react-quickstart/mcbcapp/frontend/src/components/Header.js
@@ -15,10 +15,7 @@ import ListItemText from "@material-ui/core/ListItemText";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import { spacing } from "@material-ui/system";
 
 const drawerWidth = 250;
 const drawerIconWidth = 28;
