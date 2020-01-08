f47a387bb8014b7794d592c98bd72c2159f3e168
Dragon <dragon88power@gmail.com>
2020-01-08 15:52:04 +1100

    Login Page with background, need to gimp the image

--- django-drf-react-quickstart/mcbcapp/frontend/src/views/LoginPage.js
+++ django-drf-react-quickstart/mcbcapp/frontend/src/views/LoginPage.js
@@ -4,28 +4,23 @@ import { withStyles } from "@material-ui/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import InputAdornment from '@material-ui/core/InputAdornment';
+import Grid from '@material-ui/core/Grid';
 
 const loginStyles = theme => ({
   root: {
-    backgroundImage:
-      "url(" + "/static/assets/images/backgroundProfilePage.jpg" + ")",
+    backgroundImage: "url(" + "/static/assets/images/backgroundLogin.png" + ")",
     backgroundSize: "cover",
     backgroundPosition: "center",
     textAlign: "center",
     display: "flex",
     flexDirection: "column"
   },
-  loginBox: {
-    backgroundColor: "white",
-    borderRadius: "10%",
-    color: "red"
+  logindiv: {
+    marginTop: "12%",
+    marginBottom: "15%"
   },
-  loginButton:{
-    backgroundColor: "red",
-    borderRadius: "40%", 
-    color: "white",
-    textAlign: "center",
-    width: "80%"
+  logintext: {
+    width: "25%"
   }
 });
 
@@ -35,54 +30,60 @@ class LoginPage extends React.Component {
 
     return (
       <div className={classes.root}>
-        <div>
-          <img
-            style={{ marginTop: "1%" }}
-            src="/static/assets/icons/RosterIcon.png" // LOGO TO ADD
-          />
+        <div className={classes.logindiv}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <img
+                style={{ marginTop: "1%" }}
+                src="/static/assets/icons/RosterIcon.png" 
+              /> 
+            </Grid>          
+            <Grid item xs={12}>
+              <TextField
+                className={classes.logintext}
+                id="outlined-required"
+                label="username"
+                defaultValue=""
+                variant="outlined"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <img style={{ marginTop: "1%" }} src="/static/assets/icons/ProfileIcon.png"/>
+                    </InputAdornment>
+                  ),
+                }}/>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  className={classes.logintext}
+                  id="outlined-password-input"
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <img style={{ marginTop: "1%" }} src="/static/assets/icons/PasswordIcon.png"/>
+                      </InputAdornment>
+                    ),
+                }}/>  
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                 className={classes.logintext}
+                 style={{
+                  borderRadius: "100px",
+                  margin: "auto",
+                  paddingLeft: "15%",
+                  paddingRight: "15%"}}  
+                  variant="contained" 
+                  color="primary">
+                  Login
+                </Button>
+              </Grid>
+            </Grid> 
+          </div>       
         </div>
-        <div>
-          <div className={classes.loginBox}>  
-            <TextField
-            id="outlined-required"
-            label="username"
-            defaultValue=""
-            variant="outlined"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <img style={{ marginTop: "1%" }} src="/static/assets/icons/ProfileIcon.png"/>
-                </InputAdornment>
-              ),
-            }}/>
-          </div>
-          <div className={classes.loginBox}>
-            <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            variant="outlined"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <img style={{ marginTop: "1%" }} src="/static/assets/icons/PasswordIcon.png"/>
-                </InputAdornment>
-              ),
-            }}/>        
-            
-            
-          </div>
-          <Button style={{
-            borderRadius: "100px",
-            margin: "auto",
-            paddingLeft: "15%",
-            paddingRight: "15%"}}  
-            variant="contained" 
-            color="primary">
-            Login
-         </Button>
-        </div>
-      </div>
     );
   }
 }
